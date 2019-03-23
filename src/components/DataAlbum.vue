44ea67df89dd4d3f03c00629f7e402f6ef03b371
Alan Jenkins <alanjenkins1978@gmail.com>
2019-03-23 12:52:14 +0000

    Added missing data component

---
+++ src/components/DataAlbum.vue
@@ -0,0 +1,20 @@
+<script>
+import Albums from '~/data/Albums.yml'
+
+export default {
+  computed: {
+    albums() {
+      return Albums.items
+    },
+    album() {
+      return Albums.items.find(item => item.url == this.$route.fullPath)
+    }
+  },
+  render() {
+    return this.$scopedSlots.default({
+      album: this.album,
+      albums: this.albums
+    })
+  } 
+}
+</script>
