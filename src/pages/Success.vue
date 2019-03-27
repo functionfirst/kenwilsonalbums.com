b1adcd4cab81831d569528070f0f57f4882dcb8d
Alan Jenkins <alanjenkins1978@gmail.com>
2019-03-27 18:21:58 +0000

    Updated Paypal Art button with dropdown for canvas type

---
+++ src/pages/Success.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <div class="max-w-sm">
+      <h1 class="font-thin leading-loose text-6xl text-lighter">Thank you for getting in touch!</h1>
+      <p class="text-base text-2xl font-thin antialised my-2">Ken will get back in touch with you as soon as he can.</p>
+
+      <p class="leading-normal text-lighter my-6">
+        In the meantime, feel free to browser the rest of the website.
+      </p>
+
+      <g-link to="/" class="mt-4 inline-block border border-dark text-dark antialiased font-bold text-sm px-8 py-3 no-underline rounded-full hover:text-lighter hover:border-lighter">
+      Back to home
+      </g-link>
+    </div>
+  </Layout>
+</template>
