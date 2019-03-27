a362feff0fb9401205dda6e768156f6ca30b4d58
Alan Jenkins <alanjenkins1978@gmail.com>
2019-03-27 18:18:20 +0000

    Updated custom 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,6 +1,16 @@
 <template>
   <Layout>
-    <h1>404</h1>
-    <p>Something went wrong..</p>
+    <div class="max-w-sm">
+      <h1 class="font-thin leading-loose text-6xl text-lighter">Something went wrong</h1>
+      <p class="text-base text-2xl font-thin antialised my-2">We couldn't find the page you were looking for.</p>
+
+      <p class="leading-normal text-lighter my-6">
+        You can either return to the previous page, visit my homepage or select a new page from the list above.
+      </p>
+
+      <g-link to="/" class="mt-4 inline-block border border-dark text-dark antialiased font-bold text-sm px-8 py-3 no-underline rounded-full hover:text-lighter hover:border-lighter">
+      Back to home
+      </g-link>
+    </div>
   </Layout>
 </template>
