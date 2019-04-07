a112a75441b7fec8f92ee7a11a516524797465d5
Alan Jenkins <alanjenkins1978@gmail.com>
2019-04-07 22:15:22 +0100

    Fixed relative path for Art

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-wrap items-stretch">
     <div class="flex items-start xs:w-1/2 sm:w-1/3 md:w-1/4 mb-2" v-for="({ node }) in items" :key="node.id">
-      <g-link :to="`art/${node.slug}`" class="hover:opacity-75 relative m-2 flex flex-col border border-base p-2 hover:border-dark no-underline">
+      <g-link :to="`/art/${node.slug}`" class="hover:opacity-75 relative m-2 flex flex-col border border-base p-2 hover:border-dark no-underline">
         <div class="flex-1 relative">
           <div class="absolute pin"></div>
           <g-image :src="node.image" :alt="`Image of ${node.title}`" />
