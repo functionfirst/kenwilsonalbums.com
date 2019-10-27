16fb95f6748f6ac3753b6c2cb8aff826763ae726
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2019-10-27 21:40:57 +0000

    Added Pacifico as self-hosted font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import DefaultLayout from '~/layouts/Default.vue'
 require('~/assets/css/style.sass')
+require('typeface-pacifico')
 
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
