c1b29351fc51d95d890f2a6429c781140eaad254
Alan Jenkins <alanjenkins1978@gmail.com>
2020-02-28 18:24:58 +0000

    Fixes album page links

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <div class="md:flex items-start mb-12" v-for="({ node }) in $static.albums.edges" :key="node.id">
-      <g-link :to="node.slug">
+    <div
+      class="md:flex items-start mb-12"
+      v-for="({ node }) in $static.albums.edges"
+      :key="node.id"
+    >
+      <g-link :to="`/albums/${node.slug}`">
         <g-image :src="node.image" :alt="`Album image for ${node.title}`" />
       </g-link>
 
       <div class="md:ml-8 leading-normal max-w-md">
         <h2>
-          <g-link :to="node.slug" class="text-darker hover:text-dark">
-            {{ node.title }}
-          </g-link>
+          <g-link :to="`/albums/${node.slug}`" class="text-darker hover:text-dark">{{ node.title }}</g-link>
         </h2>
 
-        <p v-if="node.aside" class="my-4 text-sm italic">
-          {{ node.aside }}
-        </p>
+        <p v-if="node.aside" class="my-4 text-sm italic">{{ node.aside }}</p>
 
         <div class="content" v-html="node.content"></div>
       </div>
