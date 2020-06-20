0956ced97b581a64055d3db77e819e399b8baed6
Alan Jenkins <alanjenkins1978@gmail.com>
2020-06-20 15:38:35 +0100

    Adds a new layout for use on the homepage
    This removes the fixed width layout so the homepage sections can stretch across the full page.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import DefaultLayout from '~/layouts/Default.vue'
+import HomepageLayout from '~/layouts/Homepage.vue'
 require('~/assets/css/style.sass')
 require('typeface-pacifico')
 
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
+  Vue.component('Homepage', HomepageLayout)
 
   head.bodyAttrs = { class: 'font-sans' }
 }
