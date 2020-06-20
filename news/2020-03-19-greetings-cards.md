6c96f7b6b02cbc3bed7ec4932c4f73813294dbf4
Alan Jenkins <alanjenkins1978@gmail.com>
2020-06-20 14:56:31 +0100

    Fixes news article paths

--- news/2020-03-19-greetings-cards.md
+++ news/2020-03-19-greetings-cards.md
@@ -1,9 +1,10 @@
 ---
 title: Greetings Cards
 date: 2020-03-19T13:28:04.557Z
-slug: greetings cards
+slug: 2020-03-19-greetings-cards
 image: /images/87809745_816956995473917_5652131576616583168_o.jpg
 ---
+
 \*\*New\*\* I have recently started producing greetings cards with images of my 'artwork'. Just click onto any image, and follow the directions to purchase your cards!
 
 Thank you,
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="flex flex-wrap items-stretch">
-    <div class="md:flex items-start mb-12" v-for="({ node }) in $static.allNewsPost.edges" :key="node.id">
-      <g-link :to="node.slug">
-        <g-image :src="node.image" :alt="`News image for ${node.title}`" />
+    <div
+      class="md:flex items-start mb-12"
+      v-for="({ node }) in $static.allNewsPost.edges"
+      :key="node.id"
+    >
+      <g-link :to="`news/${node.slug}`">
+        <g-image
+          :src="node.image"
+          :alt="`News image for ${node.title}`"
+        />
       </g-link>
 
       <div class="md:ml-8 leading-normal max-w-md">
         <h2>
-          <g-link :to="node.slug" class="text-darker hover:text-dark">
+          <g-link
+            :to="`news/${node.slug}`"
+            class="text-darker hover:text-dark"
+          >
             {{ node.title }}
           </g-link>
         </h2>
 
-        <div class="content" v-html="node.content"></div>
+        <div
+          class="content"
+          v-html="node.content"
+        ></div>
       </div>
     </div>
   </div>
