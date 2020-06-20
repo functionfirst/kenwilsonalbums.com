f10f9619cfb73388e9142d1be43255932e8bcbaf
Alan Jenkins <alanjenkins1978@gmail.com>
2020-06-20 20:07:44 +0100

    Increase leading on about page to improve legibility

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,12 +2,12 @@
   <Layout>
     <h1 class="leading-loose font-normal">About Ken Wilson</h1>
 
-    <div class="md:flex">
+    <div class="md:flex leading-loose">
       <div class="md:w-32">
-        <h2 class="leading-normal font-normal mr-12 leading-normal my-2 text-dark">Music</h2>
+        <h2 class="font-normal mr-12 my-2 text-dark">Music</h2>
       </div>
-      <div class="leading-normal md:max-w-sm">
-        <p class="my-4">
+      <div class="md:max-w-sm">
+        <p class="my-6">
           Born in Billingham Teesside in nineteen sixty, one of eight siblings, five brothers and three sisters.
           Sadly our sister Josephine died in infancy. I attended school in Billingham, where I developed a passion
           for sports and art, which continued until my early twenties. I became involved in the folk scene in the
@@ -20,7 +20,7 @@
           this and my work and family commitments art work took a back seat at this point.
         </p>
 
-        <p class="my-4">
+        <p class="my-6">
           I have thoroughly enjoyed being part of the folk scene for the past forty years, both within the
           family group and also as a solo performer. During that time I/we have performed at The Royal Albert
           Hall in 2011 as part of the BBC Proms. We have also worked and performed with Sting on
@@ -35,20 +35,16 @@
         <p>
           In 2017 The Wilson Family were awarded the prestigious English Folk Dance and Song Society`s
           Gold Badge for services to the folk scene. In 2017 I also released an album named
-          <a
-            href="/albums/heres-a-health-to-the-company"
-          >
+          <a href="/albums/heres-a-health-to-the-company">
             "Here`s a Health to
             the Company"
           </a> with my close friend and collaberator Jim McFarland, whom I had been singing with since
           2013. I have released 2 solo albums, the first being
-          <a
-            href="/albums/not-before-time"
-          >"Not Before Time"</a> in 2007, in 2018 I released my CD
+          <a href="/albums/not-before-time">"Not Before Time"</a> in 2007, in 2018 I released my CD
           <a href="/albums/portraits">"Portraits"</a> which has been well received, with great reviews.
         </p>
       </div>
-      <div class="mt-4 md:ml-24">
+      <div class="mt-6 md:ml-24">
         <g-image
           width="400"
           class="block w-full"
@@ -58,34 +54,32 @@
       </div>
     </div>
 
-    <div class="md:flex mt-8">
+    <div class="md:flex mt-8 leading-loose">
       <div class="md:w-32">
-        <h2 class="leading-normal font-normal mr-12 leading-normal my-2 text-dark">Artwork</h2>
+        <h2 class="mr-12 my-2 text-dark">Artwork</h2>
       </div>
-      <div class="leading-normal md:max-w-sm">
-        <p class="my-2">
+      <div class="md:max-w-sm">
+        <p class="my-6">
           In 2012 I began to have health problems, which resulted in me having a pacemaker fitted in 2013.
           However, my health continued to deteriorate, resulting in my having to spend some time on sick leave
           from work.
           <em>A further operation in 2018 has led to an improvement in my health.</em>
         </p>
 
-        <p class="my-2">
+        <p class="my-6">
           It was during this period that I began to sketch again after thirty five years. At first, drawing family
           members, then portraits of people on the folk scene. I have carried on doing this, and have now started
           to sketch a wide range of subjects, including specific comissions, all of which have been very well
           received. Most of my past and future
-          <a
-            href="/art"
-          >artwork</a> can be found on this site and is available to purchase.
+          <a href="/art">artwork</a> can be found on this site and is available to purchase.
         </p>
 
-        <p class="my-2">
+        <p class="my-6">
           Cheers,
           <br />Ken
         </p>
       </div>
-      <div class="mt-4 md:ml-24">
+      <div class="mt-6 md:ml-24">
         <g-image
           width="400"
           class="block w-full"
