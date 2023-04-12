9221a77fcd3ca30b904a3f2cdb8a4f4109165515
Alan Jenkins <alanjenkins1978@gmail.com>
2023-04-12 18:53:38 +0100

    Add fix for date

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import DefaultLayout from '~/layouts/Default.vue'
 import HomepageLayout from '~/layouts/Homepage.vue'
-require('~/assets/css/style.sass')
-require('typeface-pacifico')
+import 'typeface-pacifico'
+import '~/assets/css/style.sass'
 
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
